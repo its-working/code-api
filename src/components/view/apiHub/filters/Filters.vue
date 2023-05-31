d17b30a7dd7699b4430a0cb8e400344b1ae4fd57
Manav Verma <78682260+manaver@users.noreply.github.com>
2023-05-31 13:39:28 +0530

    bug fixed

--- src/components/view/apiHub/filters/Filters.vue
+++ src/components/view/apiHub/filters/Filters.vue
@@ -1,19 +1,19 @@
 <template>
-  <div
-    class="filter-position">
+  <div class="filter-position">
     <div class="wrapper">
-      <button class="hidden md:flex text-white self-end text-3xl -mt-2 mb-3 duration-300 hover:scale-110">
+      <button
+        class="hidden md:flex text-white self-end text-3xl -mt-2 mb-3 duration-300 hover:scale-110"
+      >
         <ion-icon name="chevron-back-circle-outline"></ion-icon>
       </button>
-      
-      <div class="heading ms-5 md:ms-0">
-        Filters
+
+      <div class="heading ms-5 md:ms-0">Filters</div>
+      <div @click="toggleFilter" class="filterToggle text-3xl me-3 md:hidden">
+        <ion-icon name="close-circle-outline"></ion-icon>
       </div>
-      <div @click="toggleFilter" class="filterToggle text-3xl me-3 md:hidden"><ion-icon name="close-circle-outline"></ion-icon></div>
     </div>
 
     <TagsComponent />
-  
   </div>
 </template>
 
