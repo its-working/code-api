93d4591b982f1a2ec9be8357fce85b0612b4cb5b
Manav Verma <78682260+manaver@users.noreply.github.com>
2023-06-01 20:04:30 +0530

    filter toggle for desktop added

--- src/components/view/apiHub/filters/Filters.vue
+++ src/components/view/apiHub/filters/Filters.vue
@@ -1,19 +1,31 @@
 <template>
-  <div class="filter-position">
-    <div class="wrapper">
+  <div
+    class="filter-position duration-200"
+    :class="{ toggledFilterPos: isFilterToggled, FilterPos: !isFilterToggled }"
+  >
+    <div class="closeBtn w-full flex justify-end">
       <button
-        class="hidden md:flex text-white self-end text-3xl -mt-2 mb-3 duration-300 hover:scale-110"
+        @click="toggleFilter"
+        :class="{ 'rotate-180 ': isFilterToggled }"
+        class="hidden transition-all duration-200 md:flex h-full text-white text-3xl hover:scale-110"
       >
         <ion-icon name="chevron-back-circle-outline"></ion-icon>
       </button>
+    </div>
 
-      <div class="heading ms-5 md:ms-0">Filters</div>
-      <div @click="toggleFilter" class="filterToggle text-3xl me-3 md:hidden">
+    <div class="flex flex-row justify-between items-center">
+      <div class="filter-heading ms-5 md:ms-0">Filters</div>
+      <div @click="toggleFilterMobile" class="filterToggle text-3xl me-3 md:hidden">
         <ion-icon name="close-circle-outline"></ion-icon>
       </div>
     </div>
 
-    <TagsComponent />
+    <!-- Filter components -->
+    <div class="wrapper" :class="{ wrapperToggled: isFilterToggled }">
+      <TagsComponent />
+      <TagsComponent />
+      <TagsComponent />
+    </div>
   </div>
 </template>
 
@@ -22,13 +34,33 @@ import TagsComponent from "./Tags.vue";
 
 export default {
   name: "FilterComponent",
+  data() {
+    return {
+      isFilterToggled: false,
+    };
+  },
   components: {
     TagsComponent,
   },
   methods: {
-    toggleFilter() {
+    toggleFilterMobile() {
       this.$emit("isFilterHidden", true);
+    },
+    toggleFilter() {
+      this.isFilterToggled = !this.isFilterToggled;
     },
   },
 };
 </script>
+
+<style>
+.toggledFilterPos {
+  @apply overflow-hidden md:w-[100px];
+}
+.wrapperToggled {
+  @apply blur;
+}
+.FilterPos {
+  @apply overflow-y-auto md:w-full;
+}
+</style>
