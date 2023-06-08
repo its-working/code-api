fb961196933d43b91047b74cf6fbce18069d0733
Manav Verma <78682260+manaver@users.noreply.github.com>
2023-06-08 21:13:18 +0530

    Code API python compiler added

--- src/views/CodeApi.vue
+++ src/views/CodeApi.vue
@@ -26,19 +26,38 @@
           @beforeChange="beforeChange"
         />
       </div>
+      <div class="btn text-2xl">Save</div>
 
       <!-- URL IF GET REQUEST -->
       <div
-        class="font-bold text-center p-3 m-3 border-2 border-slate-800 rounded-md overflow-x-auto"
+        class="font-bold text-center p-3 m-3 border-2 border-slate-800 rounded-md overflow-x-auto w-full"
       >
-        URL: https://api.example.com/mergeSortAPI?arg1=[1,3,2]&arg2=[4,6,5]&arg3=1
+        <h4 class="mb-4 text-xl">Test Your API:</h4>
+        <div
+          v-for="x in parametersCount"
+          :key="x"
+          class="testCase flex flex-row items-center w-3/4 md:w-2/4 lg:w-2/5 mx-auto"
+        >
+          <label style="white-space: nowrap">Parameter Value: {{ x }}</label>
+          <input type="text" v-model="testCase[x - 1]" class="ms-3 form-input" />
+        </div>
+        <div
+          class="url bg-slate-800 p-2 overflow-y-auto rounded-lg my-3 max-w-[90%] mx-auto scrollbar-customized"
+          style="white-space: nowrap"
+        >
+          URL: https://{{ domain }}/{{ getUserName }}/{{ apiName }}?{{ getUrlParm }}
+        </div>
+        <div @click="executeCode" class="btn w-fit mx-auto mt-5 cursor-pointer">
+          Run Code
+        </div>
       </div>
 
       <div
         class="RunConsole p-2 w-full bg-slate-950 my-4 rounded mx-auto overflow-x-auto"
         style="min-height: 200px"
       >
-        <h6 class="text-center font-bold underline underline-offset-8">Test Cases</h6>
+        <h6 class="text-center font-bold underline underline-offset-8 mb-4">Output</h6>
+        <p class="px-3" v-html="output"></p>
       </div>
     </div>
   </div>
@@ -59,6 +78,7 @@ import "codemirror/theme/dracula.css";
 //Syntax json
 import dataTypeJson from "/src/data/dataType.json";
 import codeSyntaxJson from "/src/data/codeSyntax.json";
+import axios from "axios";
 
 export default {
   name: "codeApi",
@@ -68,9 +88,12 @@ export default {
       value: localStorage.getItem("ApiDetails"),
       parametersCount: 0,
       parameterNames: [],
+      testCase: [],
       apiName: "",
       apiDescription: "",
       codeSyntax: "",
+      code: "",
+      output: "",
       returnType: "",
       apiLanguage: "",
       dataTypeJson: dataTypeJson,
@@ -94,8 +117,44 @@ export default {
       this.codeSyntax = syntax;
       return syntax;
     },
+    setCode() {
+      this.code = this.codeSyntax;
+    },
+    executeCode() {
+      // Make an HTTP POST request to the PHP backend endpoint
+      this.setCode();
+      const formData = new FormData();
+      formData.append("code", this.code);
+
+      axios
+        .post("https://codeapi.aslamtourandtravel.xyz/execute.php", formData)
+        .then((response) => {
+          // Handle the response and update the output variable
+          this.output = response.data;
+          console.log(2);
+        })
+        .catch((error) => {
+          // Handle any errors
+          console.error(error);
+          console.log(3);
+        });
+    },
   },
   computed: {
+    domain() {
+      return window.location.hostname;
+    },
+    getUserName() {
+      return localStorage.getItem("username");
+    },
+    getUrlParm() {
+      return this.parameterNames
+        .map((name, i) => {
+          const value = this.testCase[i] === undefined ? "" : this.testCase[i];
+          return `${name}=${value}`;
+        })
+        .join("&");
+    },
     codeTemplate() {
       const syntax = codeSyntaxJson[this.apiLanguage];
       if (syntax) {
